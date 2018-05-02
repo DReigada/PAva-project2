46f94c0c5c50b4f7ae90181dd9894796887a03c6
Daniel Reigada <daniel.reigada@gmail.com>
2018-05-02 19:39:15 +0100

    Add method to find tokens in the beginning of a string

--- preprocess.rkt
+++ preprocess.rkt
@@ -1,13 +1,36 @@
 #lang racket
 (provide add-active-token def-active-token); process-string)
-(provide get-token-list); TODO remove this
+(provide get-token-list find-token); TODO remove this
 
-(define token-list '())
+(define *token-list* '())
 
 (define (add-active-token token function)
-    (set! token-list (cons (cons token function) token-list)))
+    (set! *token-list* (cons (cons token function) *token-list*)))
+
+; (define (process-string str)
+;     )
+
+(define (find-token str token-list)
+    (and
+        (not (null? token-list))
+        (let ([token-pair (car token-list)])
+            (if (starts-with? str (car token-pair))
+                (cdr token-pair)
+                (find-token str (cdr token-list))
+            ))))
+
+(define (starts-with? str expr)
+    (let   ([expr-length (string-length expr)]
+            [str-length (string-length str)])
+        (and 
+            (>= str-length expr-length)
+            (string=?
+                (substring str 0 (string-length expr))
+                expr))))
+
 
 (define-syntax-rule (def-active-token token parameters body)
     (add-active-token token (lambda parameters body)))
 
-(define (get-token-list) token-list)
+
+(define (get-token-list) *token-list*)
