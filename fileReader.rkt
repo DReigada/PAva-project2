9ff6f59b40055b38209402b595192eb30c412938
Daniel Reigada <daniel.reigada@gmail.com>
2018-05-10 16:16:00 +0100

    Add auto tester

---
+++ fileReader.rkt
@@ -0,0 +1,44 @@
+#lang racket
+ 
+(require rackunit "preprocess.rkt")
+
+(define (process-file file-path output-suffix)
+    (display-to-file
+        (process-string (file->string file-path #:mode 'text))
+        (string-append (path->string file-path) output-suffix)
+        #:mode 'text #:exists 'replace))
+
+
+(define (process-directory directory-path input-suffix output-suffix)
+    (for-each
+        (lambda (file-path) (process-file file-path output-suffix))
+        (filter
+            (lambda (file-path) (string-suffix? (path->string file-path) input-suffix))
+            (directory-list directory-path #:build? #t))))
+
+
+(define (verify-file expected-file-path actual-file-path)
+    (check-equal? (file->string expected-file-path) (file->string actual-file-path) actual-file-path))
+
+(define (verify-directory directory-path)
+    (for-each
+        (lambda (input-file-path)
+            (verify-file 
+                (regexp-replace #px"\\.in$" (path->string input-file-path) ".out")
+                (regexp-replace #px"\\.in$" (path->string input-file-path) ".in.out")))
+        (filter
+            (lambda (file-path) (string-suffix? (path->string file-path) ".in"))
+            (directory-list directory-path #:build? #t))))
+
+
+
+
+
+(define (process-and-verify-directory directory)
+    (process-directory directory ".in" ".out")
+    (verify-directory directory))
+
+(process-and-verify-directory "20180507app2tests/simple/string-interpolation")
+(process-and-verify-directory "20180507app2tests/simple/type-alias")
+(process-and-verify-directory "20180507app2tests/simple/mixed-tokens")
+(process-and-verify-directory "20180507app2tests/simple/type-inference")
